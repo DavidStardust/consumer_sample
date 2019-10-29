4977614ffc080ccd31f52640cb7701c8477fdac5
David Magalhães <david.magalhaes90@hotmail.com>
2019-10-29 11:30:10 -0300

    Implementação da interface

---
+++ src/application/Program.java
@@ -0,0 +1,27 @@
+//Fazer um programa que, a partir de uma lista de produtos, aumente o
+//preço dos produtos em 10%.
+
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+import util.PriceUpdate;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		
+		list.forEach(new PriceUpdate());
+
+		list.forEach(System.out::println); //reference method
+	}
+}
